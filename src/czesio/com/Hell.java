5b34daeaa004ac525ae3eb7328c7b76562cbf734
JA <JA@Piwko>
2010-08-27 18:24:43 +0000

    Initial

---
+++ src/czesio/com/Hell.java
@@ -0,0 +1,13 @@
+package czesio.com;
+
+public class Hell {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hell o world !!!1111111");
+	}
+
+}
